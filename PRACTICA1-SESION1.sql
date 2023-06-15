0010afa9e8570081c2975d17908057c7bc02a106
bq0313 <felix.dasilva.bakabo@alumnos.upm.es>
2023-06-16 01:33:59 +0200

    ni de coña creo yo las tablas a mano

--- PRACTICA1-SESION1.sql
+++ PRACTICA1-SESION1.sql
@@ -1,5 +1,6 @@
 SELECT * FROM practica1bda_tablaunica.tablaunica;
 
+-- SESION 1
 /*1. A partir del conjunto de dependencias funcionales (L), completar los requisitos del sistema en lenguaje natural. ¿Puede servir un local a clientes de fuera de su zona? ¿Son
 los descuentos acumulables, es decir, puedo aplicar dos descuentos distintos al mismo pedido? Indica que dependencias funcionales o la ausencia de estas te permiten
 responder a las preguntas anteriores. */
@@ -13,11 +14,16 @@ encontradas para esta tabla. */
 -- RESPUESTA
 -- CLAVES: CLAVE(IPM),CLAVE(LFPM),CLAVE(EFPM),CLAVE(UFPM),CLAVE(NFPM)
 -- ESTA EN 1FN Dado que E X->Y / X es un subconjunto de la clave L F -> I E B H,
+-- SOLO SE PUEDE CREAR UNA CLAVE PRINCIPAL
 ALTER TABLE tablaunica ADD PRIMARY KEY (pedidoID,producto,medida);
-ALTER TABLE tablaunica ADD PRIMARY KEY (localID,fecha_hora,producto,medida);
-ALTER TABLE tablaunica ADD PRIMARY KEY  (clienteID,fecha_hora,producto,medida);
-ALTER TABLE tablaunica ADD PRIMARY KEY  (usuario,fecha_hora,producto,medida);
-ALTER TABLE tablaunica ADD PRIMARY KEY  (nombre_local,fecha_hora,producto,medida);
+-- ALTER TABLE tablaunica ADD PRIMARY KEY (localID,fecha_hora,producto,medida);
+-- ALTER TABLE tablaunica ADD PRIMARY KEY  (clienteID,fecha_hora,producto,medida);
+-- ALTER TABLE tablaunica ADD PRIMARY KEY  (usuario,fecha_hora,producto,medida);
+-- ALTER TABLE tablaunica ADD PRIMARY KEY  (nombre_local,fecha_hora,producto,medida);
+CREATE INDEX key_segunda ON tablaunica (localID,fecha_hora,producto,medida);
+CREATE INDEX key_tercera ON tablaunica (clienteID,fecha_hora,producto,medida);
+CREATE INDEX key_cuarta ON tablaunica (usuario,fecha_hora,producto,medida);
+CREATE INDEX key_quinta ON tablaunica (nombre_local,fecha_hora,producto,medida);
 
 /*3. Resolver en SQL las siguientes consultas: */
 /*a. Obtener el número de clientes que han realizado pedidos entregados en Local.*/
@@ -67,13 +73,13 @@ value(223,'Local','maria1',3,'La Primera','Luna',3,1,'Pizza Carbonara','Familiar
 /*5. Realizar los siguientes cambios en los datos mediante SQL explicando los problemas y anomalías encontradas: */
 /*a. Cambiar la zona de la calle Sol a valor 4.*/
 -- Error safe mode 
+-- No se han producido anomalias
 update tablaunica
-set zona_local=4
-where calle_local ='Sol';
-/*b. Debido a un error, se debe cancelar el pedido 170, eliminando los datos de dicho pedido.*/ 
-
-
-
-
+set zona_cliente=4
+where calle_cliente ='Sol';
+/*b. Debido a un error, se debe cancelar el pedido 170, eliminando los datos de dicho pedido.*/
+-- ANOMALIA: Se han perdido los datos del usuario marta23
+delete from tablaunica
+where pedidoID=170;
 
 
